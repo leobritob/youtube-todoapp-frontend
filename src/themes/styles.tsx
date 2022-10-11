535ae12c10a8bcd6340ab3d767c5e0c0d66bb5d7
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2022-10-11 16:08:36 -0300

    chore: update readme file

--- src/themes/styles.tsx
+++ src/themes/styles.tsx
@@ -2,9 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 import { ThemeType } from './theme';
 
 export const GlobalStyles = createGlobalStyle<{ theme: ThemeType }>`
-  @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap');
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap');
-
   * {
     margin: 0;
     padding: 0;
